f42c5b49b09d19f335a564b34d4f9ab7d6b7509e
sidmfkid <newskinlifestore@gmail.com>
2022-05-21 02:11:28 -0400

    mobile menu css only done

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,11 @@
 const menuButton = document.getElementById("menuButton");
 const navList = document.querySelector(".nav__list");
 
-menuButton.addEventListener("click", openMobileMenu.bind(this));
+// menuButton.addEventListener("click", openMobileMenu.bind(this));
 
-function openMobileMenu(e) {
-  e.preventDefault();
-  menuButton.classList.toggle("open");
-  navList.classList.toggle("open");
-  console.log(e);
-}
+// function openMobileMenu(e) {
+//   e.preventDefault();
+//   menuButton.classList.toggle("open");
+//   navList.classList.toggle("open");
+//   console.log(e);
+// }
