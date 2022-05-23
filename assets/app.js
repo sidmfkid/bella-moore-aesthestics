957ab1a3b37a3d981d65daa2d6305c16501faa0b
sidmfkid <newskinlifestore@gmail.com>
2022-05-22 20:35:46 -0400

    navigation/header fixed, footer started

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,20 @@
 const menuButton = document.getElementById("menuButton");
 const navList = document.querySelector(".nav__list");
 
+const subLinkList = document.querySelectorAll("#subLinkList");
+
+// subLinkList.forEach(el => {
+//     el.addEventListener('click', openSubLinks.bind(this))
+// });
+
+
+
+// function openSubLinks(e) {
+//       e.preventDefault();
+//     console.log(e.target)
+//     e.target.classList.toggle("open")
+// }
+
 // menuButton.addEventListener("click", openMobileMenu.bind(this));
 
 // function openMobileMenu(e) {
