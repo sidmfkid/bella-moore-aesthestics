b2e4fa277bc08bffe9d89acfb4aef93fa08a590a
sidmfkid <newskinlifestore@gmail.com>
2022-07-06 16:08:48 -0400

    mobile nav fixed, started animations

--- assets/app.js
+++ assets/app.js
@@ -1,66 +1,173 @@
 const menuButton = document.getElementById("menuButton");
 const navList = document.querySelector(".nav__list");
-const navShop = document.querySelector('.shopify-section.main__header');
-const nav = document.querySelector('header');
-const heros = document.querySelectorAll('.shopify-section');
+const navShop = document.querySelector(".shopify-section.main__header");
+const nav = document.querySelector("header");
+const allSections = document.querySelectorAll(".shopify-section");
 const subLinks = document.querySelectorAll("#subLinkList");
+const mobileMenuBtn = document.getElementById("menu");
+const body = document.querySelector("body");
 
-subLinks.forEach(subLink => {
-   subLink.addEventListener('click', openSubMenu.bind(this))
+mobileMenuBtn.addEventListener("change", handleMenuChange.bind(this));
+
+function handleMenuChange(e) {
+  e.preventDefault();
+
+  /* CHECKS TO SEE IF MENU OPEN AND ADDS STYLE TO STOP MENU FROM SCROLLING THE PAGE */
+  if (mobileMenuBtn.checked) {
+    body.style = "overflow: hidden";
+  } else {
+    body.style = "overflow: visible";
+  }
+}
+
+subLinks.forEach((subLink) => {
+  subLink.addEventListener("click", openSubMenu.bind(this));
 });
 
-// const prevTarget = subLinks.filter(link => link.index > 1);
-console.log(subLinks)
+console.log(subLinks);
 
 function openSubMenu(e) {
-console.log(e.target)
-    if (e.target.classList.contains('header__sub-link') || e.target.classList.contains('header__sub-link-item')) {
-        return;
-    }
-    e.preventDefault();
-    e.stopPropagation();
-    let linkArray = []
-    subLinks.forEach(link => linkArray.push(link))
+  console.log(e.target);
+  if (
+    e.target.classList.contains("header__sub-link") ||
+    e.target.classList.contains("header__sub-link-item")
+  ) {
+    return;
+  }
+  e.preventDefault();
+  e.stopPropagation();
+  let linkArray = [];
+  subLinks.forEach((link) => linkArray.push(link));
 
-    const prevTarget = linkArray.filter(link => link.classList.contains('open'))
+  const prevTarget = linkArray.filter((link) =>
+    link.classList.contains("open")
+  );
 
-    
+  if (e.currentTarget.classList.contains("open")) {
+    e.currentTarget.classList.remove("open");
+  }
+  if (!e.currentTarget.classList.contains("open")) {
+    e.currentTarget.classList.add("open");
+  }
 
-    if (e.currentTarget.classList.contains('open')) {
-        e.currentTarget.classList.remove('open')
-    } 
-    if (!e.currentTarget.classList.contains('open')) {
-        e.currentTarget.classList.add('open')
-    }
-
-    if (prevTarget[0] ) {
-        prevTarget[0].classList.remove('open')
-    }
-
+  if (prevTarget[0]) {
+    prevTarget[0].classList.remove("open");
+  }
 }
 
+let stickyOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.6,
+};
 
-
-let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: .6
-}
-
-let observer = new IntersectionObserver(toggleStickyNav, options);
-
-
+let stickyNavObserver = new IntersectionObserver(
+  toggleStickyNav,
+  stickyOptions
+);
 
 function toggleStickyNav(entries, observer) {
-
-console.log(entries)
-if (!entries[0].isIntersecting) {
-    nav.classList.add('sticky');
-    navShop.classList.add('sticky');
-} else {
-    nav.classList.remove('sticky');
-    navShop.classList.remove('sticky');
+  console.log(entries);
+  if (!entries[0].isIntersecting) {
+    nav.classList.add("sticky");
+    navShop.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+    navShop.classList.remove("sticky");
+  }
 }
+
+stickyNavObserver.observe(navShop);
+
+//* ********************************** SECTION ANIMATIONS *********************** *//
+
+let animatedOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
+let sectionsObserver = new IntersectionObserver(
+  handleAnimations,
+  animatedOptions
+);
+
+function handleAnimations(entries, observer) {
+  console.log(...entries);
+  for (let i = 0; i < entries.length; i++) {
+    entries[i].isIntersecting &&
+    entries[i].target.classList.contains("hero") === false
+      ? animateFadeIn(entries[i].target.children[0], entries[i])
+      : console.log("not intersecting");
+  }
+  checkData(...entries, [...entries]);
+}
+allSections.forEach((section) => {
+  if (
+    section.id === "shopify-section-header" ||
+    section.id === "shopify-section-footer"
+  ) {
+    return;
+  } else {
+    sectionsObserver.observe(section);
+  }
+});
+
+function checkData(entry, entries) {
+  const section = entry.target.children[1] || entry.target.children[0];
+
+  if (section.dataset.animate === "fadeIn") {
+    animateFadeIn(section, entry);
+  }
+  if (section.dataset.animate === "fadeRight") {
+    animateFadeRight(section, entry);
+  }
+  if (section.dataset.animate === "fadeLeft") {
+    animateFadeLeft(section, entry);
+  }
+  if (section.dataset.animate === "fadeUp") {
+    animateFadeUp(section, entry);
+  }
+  if (section.dataset.animate === "fadeDown") {
+    animateFadeDown(section, entry);
+  }
 }
 
- observer.observe(heros[1])
+function animateFadeIn(section, entry) {
+  if (entry.isIntersecting /*&& !section.classList.contains("fade-in")*/) {
+    section.classList.add("fade-in");
+  } else {
+    section.classList.remove("fade-in");
+  }
+}
+function animateFadeRight(section, entry) {
+  if (entry.isIntersecting /*&& !section.classList.contains("fade-in")*/) {
+    section.classList.add("fade-right");
+  } else {
+    section.classList.remove("fade-right");
+  }
+}
+
+function animateFadeLeft(section, entry) {
+  if (entry.isIntersecting /*&& !section.classList.contains("fade-in")*/) {
+    section.classList.add("fade-left");
+  } else {
+    section.classList.remove("fade-left");
+  }
+}
+
+function animateFadeUp(section, entry) {
+  if (entry.isIntersecting /*&& !section.classList.contains("fade-in")*/) {
+    section.classList.add("fade-up");
+  } else {
+    section.classList.remove("fade-up");
+  }
+}
+
+function animateFadeDown(section, entry) {
+  if (entry.isIntersecting /*&& !section.classList.contains("fade-in")*/) {
+    section.classList.add("fade-down");
+  } else {
+    section.classList.remove("fade-down");
+  }
+}
